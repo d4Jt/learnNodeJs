0ea924700af5a9622516956786b8dbdc2e709447
Gr33n <jsmsj04@gmail.com>
2022-06-06 19:19:46 +0700

    first commit

---
+++ learn.js
@@ -0,0 +1,17 @@
+// https://vi.wikipedia.org/wiki/Hypertext_Transfer_Protocol
+/*
+HTTP: phương thức truyền tải ngôn ngữ đánh dấu siêu văn bản
+
+status code
+
+request methods:
+    GET: lấy dữ liệu
+    POST: gửi dữ liệu
+    PUT / PUSH
+    DELETE
+    dùng phần body để render ra html
+
+SSR (server side rendering) & CSR (client side rendering)
+
+
+*/
