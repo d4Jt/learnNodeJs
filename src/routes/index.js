ea26fee2ebf3c7eef465df4a6aca017d9215fff5
Gr33n <jsmsj04@gmail.com>
2022-06-11 15:04:47 +0700

    Refactor: Create routes & app/controllers

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const newsRouter = require("./news");
+const siteRouter = require("./site");
+
+function route(app) {
+   // Action -> Dispatcher -> Function handler
+   // mỗi khi có thằng /news nó sẽ chọc vào newsRouter
+   // index.js (chính) -> index.js (routes) -> news.js -> NewsController.js
+   app.use("/news", newsRouter);
+
+   app.use("/", siteRouter);
+}
+
+module.exports = route;
